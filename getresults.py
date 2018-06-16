0f19e521692bd06d315756cf32ed51f82a5951f4
Shubha Swamy <shubhaswamy2@gmail.com>
2018-06-16 00:20:06 -0700

    working python file gets number

--- getresults.py
+++ getresults.py
@@ -1,6 +1,8 @@
 import requests
 import json
 from collections import OrderedDict
+import unicodedata
+import ast
 
 # api-endpoint
 
@@ -14,28 +16,38 @@ URL = "https://api.typeform.com/forms/ht2toX/responses"
 response = requests.get(URL, headers={'authorization': 'bearer HrHtpTSH1ZpVZgoK9gdVZy5m66FTgSME8yA5h25rSemq'})
 data = response.json()
 
+
 #data2 = json.loads(data.decode('utf-8'), object_pairs_hook=OrderedDict)
 #data2 =json.dumps(data)
+
 
 #print(data2)
 #response2 = requests.get(URL2)
 #data2 = response2.json()
 
-print(data)
+#print(data3)
 
 
 #with open('data.txt', 'w') as outfile:  
-#json.dump(data, outfile)
+	#json.dump(data, outfile)
 
 
 def printJSON(d): 
 	phoneNumber = ""
 	
-	phoneNumber = ""
 	for i in d['items']:
 		for x in i['answers']:
-			typeVal = x['type']
-			print(type(x[typeVal]), x[typeVal])
+			#print(" x: ", x)
+			y = x['field']['id']
+			idVal = 'JomoOraxCZNn'
+			if (y == idVal):
+				print(x['text'])
+			#typeVal = x['type']
+			#print(x)
+			#print(x['field']['text'])
+			#print(type(x[typeVal]), x[typeVal])
+
+			#print(unicodedata.normalize('NFC', x[typeVal]))
 
 
 
@@ -45,8 +57,15 @@ def printJSON(d):
 	phoneNumber = ""
 	for i in d['items']:
 		for x in i['answers']:
+			print(x['field'])
+
+			
 			typeVal = x['type']
+			#print(x)
+			#print(x['field']['text'])
 			print(type(x[typeVal]), x[typeVal])
+
+			#print(unicodedata.normalize('NFC', x[typeVal]))
 
 	'''
 
