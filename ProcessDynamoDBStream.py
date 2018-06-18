0d982568b219c2e8733abb6bea965ce0ab9669d4
Shubha Swamy <shubhaswamy2@gmail.com>
2018-06-18 00:02:02 -0700

    removed process dynamo DB stream

--- ProcessDynamoDBStream.py
+++
@@ -1,7 +0,0 @@
-from __future__ import print_function
-
-def lambda_handler(event, context):
-    for record in event['Records']:
-        print(record['eventID'])
-        print(record['eventName'])       
-    print('Successfully processed %s records.' % str(len(event['Records'])))
